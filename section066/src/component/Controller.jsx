6b7775a865716d5ecaaac5cb2aa7c69311a6f861
DynamicRD <qlfflgkfflxjs@gmail.com>
2024-12-31 09:10:15 +0900

    Revert "복원"
    This reverts commit 405f421c1a68994364cbc4c553dc3f528cbe2e93.

--- section066/src/component/Controller.jsx
+++ section066/src/component/Controller.jsx
@@ -1,6 +1,5 @@
 import "./controller.css";
 <<<<<<< HEAD
-<<<<<<< HEAD
 import { useState } from "react";
 
 const Controller = (input) => {
@@ -14,48 +13,29 @@ const Controller = (input) => {
   }
 =======
 >>>>>>> 8e47fce8445ff7a0a14a13b15d2039973085717f
-=======
-import { useState } from "react";
 
-const Controller = ({ onClickButton }) => {
-  const [input, setInput] = useState({
-    firstNum: "",
-    secondNum: "",
-    symbol: "",
-  });
-
-  function onChange(e) {
-    console.log({ ...input, [e.target.name]: e.target.value });
-    setInput({ ...input, [e.target.name]: e.target.value });
-  }
->>>>>>> parent of 8e47fce (간단한 계산기)
-
+const Controller = ({ onChange, input, onClickCalculate }) => {
   return (
     <>
       <div>
         <input
-          //value={input.name}
-          //ref={input.firstNum}
           name="firstNum"
+          value={input.firstNum}
           onChange={onChange}
-          type="text"
+          type="number"
         />
       </div>
       <div>
         <select
-          //value={input.symbol}
           name="symbol"
-<<<<<<< HEAD
 <<<<<<< HEAD
           onChange={onChangeSymbol}
 =======
           value={input.symbol}
-=======
->>>>>>> parent of 8e47fce (간단한 계산기)
           onChange={onChange}
 >>>>>>> 8e47fce8445ff7a0a14a13b15d2039973085717f
         >
-          <option value=""></option>
+          <option value="">연산자</option>
           <option value="+">+</option>
           <option value="-">-</option>
           <option value="*">*</option>
@@ -64,12 +44,14 @@ const Controller = ({ onClickButton }) => {
       </div>
       <div>
         <input
-          //value={input.name}
-          //ref={input.secondNum}
           name="secondNum"
+          value={input.secondNum}
           onChange={onChange}
-          type="text"
+          type="number"
         />
+      </div>
+      <div>
+        <button onClick={onClickCalculate}>계산</button>
       </div>
     </>
   );
